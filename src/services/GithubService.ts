b25185e0d5356d5f4f6154b943022cd31c3b566d
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-11-02 23:51:04 +0000

    feat: add octokit rest to GithubService

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -5,6 +5,8 @@ import { OctokitInstance } from '@/@types/OctokitInstance';
 const octokit: OctokitInstance = github.getOctokit(Inputs.github.token);
 
 export default class GithubService {
+  static rest: OctokitInstance['rest'] = octokit.rest;
+
   static async graphql<T>(query: string, variables: unknown) {
     const result = await octokit.graphql(query, variables);
 
